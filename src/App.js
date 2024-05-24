68a532baf7ba658ae66594a4df8921fa2715671f
Shubham Chaurasia <82652474+shubh1118@users.noreply.github.com>
2024-05-24 13:13:01 +0530

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { TodoWrapper } from "./components/TodoWrapper";
+import { TodoWrapperLocalStorage } from "./components/TodoWrapperLocalStorage";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TodoWrapper /> */}
+      <TodoWrapperLocalStorage />
+    </div>
+  );
+}
+
+export default App;
